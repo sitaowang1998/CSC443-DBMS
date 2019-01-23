d34f387077dcf5f8bde613dafb629afb76d3c62e
Sitao Wang <sitaowang1998@outlook.com>
2019-01-23 17:47:08 -0500

    Correct the argument parsing

--- assignment1/load_database.sh
+++ assignment1/load_database.sh
@@ -3,8 +3,9 @@
 # script may accept one command line argument as the name of csv file
 if test "$#" -gt 1; then
 	echo "Illegal number of parameters"
+	exit 1
 elif test "$#" -eq 1; then
-	filename=$1
+	filename="$1"
 else
 	filename="500000 Records.csv"
 fi
@@ -12,4 +13,5 @@ fi
 # find the max length for each column
 # assume that empID is the first column
 # read the first line to check the number of columns
+read -r line < "$filename"
 
