00ca9eebc0c376c699948e04a6f99ac479cabc63
Sitao Wang <sitaowang1998@outlook.com>
2019-01-23 23:19:30 -0500

    Added function to find max length of all columns

--- assignment1/load_database.py
+++ assignment1/load_database.py
@@ -1,20 +1,35 @@
 import os
 import sys
 
+def find_max_length(fileName):
+    """
+    Find the max length of each column in a csv file.
+    """
+    file = open(fileName)
 
+    # Read the first line to get the number of columns
+    line = file.readline()
+    columns = line.split(',')
+    max_length = [0] * len(columns)
 
+    # Scan the rest of file to find the max length
+    line = file.readline()
+    while line:
+        counts = map(lambda item: len(item), line.split(','))
+        max_length = map(min, max_length, counts)
+        line = file.readline()
 
+    return (columns, max_length)
 
 
 # start of main
 if __name__ == "__main__":
-    # check command line argument
-    # may accept one command line argument of csv file name, default "500000 Records.csv"
+    # Check command line arguments
+    # May accept one command line argument of csv file name, default "500000 Records.csv"
     fileName = "500000 Records.csv"
     if len(sys.argv) > 2:
         print("Invalid number of arguments. May accept one argument of file name.")
     elif len(sys.argv) == 2:
         fileName = sys.argv[1]
     
-    file = open(fileName)
 
