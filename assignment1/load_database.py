59791b974e8b873bd6110ad3b28d8d3f706f0237
Sitao Wang <sitaowang1998@outlook.com>
2019-01-23 21:57:10 -0500

    Added command line argument handling

--- assignment1/load_database.py
+++ assignment1/load_database.py
@@ -1,1 +1,20 @@
 import os
+import sys
+
+
+
+
+
+
+# start of main
+if __name__ == "__main__":
+    # check command line argument
+    # may accept one command line argument of csv file name, default "500000 Records.csv"
+    fileName = "500000 Records.csv"
+    if len(sys.argv) > 2:
+        print("Invalid number of arguments. May accept one argument of file name.")
+    elif len(sys.argv) == 2:
+        fileName = sys.argv[1]
+    
+    file = open(fileName)
+
