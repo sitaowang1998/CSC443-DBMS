62fff2e2ede5b094c43c8217d19ab9496d8d11f2
Sitao Wang <sitaowang1998@outlook.com>
2019-02-05 19:06:01 -0500

    Added byte_to_entry method and calls to it

--- assignment1/cell.py
+++ assignment1/cell.py
@@ -1,4 +1,5 @@
 import os
+import struct
 
 from header import DHeader
 from helper import Reader
@@ -47,7 +48,7 @@ class TableLeafCell(Cell):
             self.payload = db.read(self.payload_size)
 
     
-class RecordFormat:
+class Record:
 
     @staticmethod
     def serial_type_to_size(serial_type):
@@ -61,6 +62,24 @@ class RecordFormat:
         if serial_type >= 13 and serial_type % 2 == 1:
             return int((serial_type - 13) / 2)
 
+    @staticmethod
+    def bytes_to_entry(serial_type, byte):
+        if serial_type == 0:
+            return None
+        if serial_type >= 1 and serial_type <= 6:
+            print(byte)
+            return int.from_bytes(byte, byteorder='big', signed=True)
+        if serial_type == 7:
+            return struct.unpack('d', byte)
+        if serial_type == 8:
+            return 0
+        if serial_type == 9:
+            return 1
+        if serial_type == 10 or serial_type == 11:
+            return None
+        if serial_type == 12 or 13:
+            return byte.decode('utf-8')
+
 
     def __init__(self, barray):
         self.header_size, offset = Reader.read_varint_string(barray)
@@ -69,17 +88,17 @@ class RecordFormat:
             serialType, newOffset = Reader.read_varint_string(barray[offset:])
             self.serial_types.append(serialType)
             offset = offset + newOffset
-        self.column_sizes = [RecordFormat.serial_type_to_size(serial_type) for serial_type in self.serial_types]
+        self.column_sizes = [Record.serial_type_to_size(serial_type) for serial_type in self.serial_types]
         self.record = []
         offset = self.header_size
         for i in range(len(self.column_sizes)):
-            self.record.append(barray[offset : offset + self.column_sizes[i]])
+            self.record.append(Record.bytes_to_entry(self.serial_types[i], barray[offset : offset + self.column_sizes[i]]))
             offset = offset + self.column_sizes[i]
 
 # main for testing
 if __name__ == "__main__":
     
     barray = b"&\x03\x17#\x0f'\x0fW?='!#\x17\x11!\x11\x11\x15\x11\x1f\x13\x11\x1f\x13\x17\x19\x13#%A9A\x11\x17\x1f++\r\x14{Ms.  Hermila    JSuhr         Fhermila.suhr@gmail.com               Todd Suhr                Cathrine Suhr           Hinojosa     9/4/1992  04:29:56 AM24.91579/9/2014  Q3H220149 SeptemberSep9 Tuesday  Tue2.88 16899112%275-17-8844479-539-4593Peach Orchard             Clay                  Peach Orchard             AR72453South    hjsuhr         oZ%{<6wN!A     "
-    r = RecordFormat(barray)
+    r = Record(barray)
     print(r.header_size, r.column_sizes)
     print(r.record)
