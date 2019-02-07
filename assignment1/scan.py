fa5a25d07fe8ea5ee96d957f4687aa0dcba3a6aa
Sitao Wang <sitaowang1998@outlook.com>
2019-02-07 10:23:06 -0500

    Added TableTreeScanner in scan.py

--- assignment1/scan.py
+++ assignment1/scan.py
@@ -4,13 +4,23 @@ from header import DHeader
 from page import Page, BTreePage, FirstPage
 from cell import Record
 
-class Scanner:
+class TableTreeScanner:
     """
-    A iterator that iterate through all components in the database
+    An iterator of all the records of a table B tree.
     """
-    def __init__(self, db):
-        # Read database header and first page
-        self.dheader = DHeader(db)
+    def __init__(self, dheader, root_page_no, db):
+        self.dheader = dheader
+        self.root_page_no = root_page_no
+        self.db = db
+        # A stack that store pair of page number and the current index of pointer
+        self.status = [(root_page_no, -1)]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        
+
 
 
 # main for testing
