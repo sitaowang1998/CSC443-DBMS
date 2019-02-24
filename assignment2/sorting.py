d8178bcc1e2b093ade6f9670530701bb6e9b0750
Sitao Wang <sitaowang1998@outlook.com>
2019-02-24 17:37:13 -0500

    Complete sortDB in sorting.py

--- assignment2/sorting.py
+++ assignment2/sorting.py
@@ -1,4 +1,6 @@
 from multiprocessing.pool import ThreadPool
+import math
+import os
 
 from record import Record
 from page import RecordPage
@@ -12,12 +14,13 @@ def sortDB(inDB, outDB, B, pSize, field):
 
 
     inFile = open(inDB, 'rb')
-    outFile = open(outDB, 'wb')
+    outFile = open(outDB, 'w+b')
+    tempFile = open('temp', 'w+b')
 
     recordsPerPage = int(pSize / Record.getSize())
 
     # Pass 0: sort all pages using B buffer pages
-    pNum = 0
+    totalPages = 0
     eof = False
     while not eof:
         buffer = []
@@ -30,17 +33,81 @@ def sortDB(inDB, outDB, B, pSize, field):
             elif len(records) < recordsPerPage:
                 eof = True
                 buffer.append(records)
-                pNum = pNum + 1
+                totalPages = totalPages + 1
             else:
                 buffer.append(records)
-                pNum = pNum + 1
+                totalPages = totalPages + 1
             i = i + 1
         # Sort each page
         p = ThreadPool(4)
         p.map(lambda index: buffer[index].sort(key=lambda r: r[field]), range(len(buffer)))
         # Write buffer pages to output file
         for page in buffer:
-            RecordPage.writeRecordPage(outFile, page, pSize)
+            RecordPage.writeRecordPage(tempFile, page, pSize)
+
+    inFile.close()
+    print("total pages: ", totalPages)
+
+    sortedSize = 1
+    inputFile = tempFile
+    outputFile = outFile
+    passNum = 1
+    while sortedSize < totalPages:
+        passNum = passNum + 1
+        inputFile.seek(0)
+        outputFile.seek(0)
+        for i in range(math.ceil(totalPages / ((B - 1) * sortedSize))):
+            start = i * (B - 1) * sortedSize
+            buffer = []
+            # Read initial pages
+            for j in range(B - 1):
+                pNum = start + j * sortedSize
+                if pNum < totalPages:
+                    page = RecordPage.readReocrdPage(inputFile, pSize, pNum)                
+                    buffer.append((page, pNum))
+            
+            target = []
+            firstElements = [b[0][0][field] for b in buffer]
+            while len(buffer) != 0:
+                minIndex = firstElements.index(min(firstElements))
+                page, pNum = buffer[minIndex]
+                target.append(page.pop(0))
+
+                # If records is empty, read next page
+                if len(page) == 0:
+                    pNum = pNum + 1
+                    # If next page belongs to another list of sorted page, stop
+                    if (pNum - start) % sortedSize == 0:
+                        buffer.pop(minIndex)
+                        firstElements.pop(minIndex)
+                    elif pNum >= totalPages:
+                        buffer.pop(minIndex)
+                        firstElements.pop(minIndex)
+                    else:
+                        page = RecordPage.readReocrdPage(inputFile, pSize, pNum)
+                        buffer[minIndex] = (page , pNum)
+                        firstElements[minIndex] = page[0][field]
+                else:
+                    firstElements[minIndex] = page[0][field]
+                
+                # If target is full, write page to outputFile
+                if len(target) == recordsPerPage:
+                    RecordPage.writeRecordPage(outputFile, target, pSize)
+                    target = []
+
+
+        sortedSize = sortedSize * (B - 1)
+        inputFile, outputFile = outputFile, inputFile
+            
+    # Clean ups
+    # Undo the last change
+    inputFile, outputFile = outputFile, inputFile
+    inputFile.close()
+    outputFile.close()
+    if outputFile.name == 'temp':
+        os.remove('sort.db')
+        os.rename('temp', 'sort.db')
+    
 
 
 
